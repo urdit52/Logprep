fa9513a69788618536c2a0bcea8a60f7f4b2602e
Piotr Pauksztelo <piotr.pauksztelo@fkie.fraunhofer.de>
2022-03-04 09:43:32 +0100

    Refactor elasticsearch output connector tests and add a new test

--- tests/unit/connector/test_elasticsearch_output.py
+++ tests/unit/connector/test_elasticsearch_output.py
@@ -1,11 +1,11 @@
-from copy import deepcopy
+import re
 from datetime import datetime
 from json import loads, dumps
 from math import isclose
 
+import arrow
 from pytest import fail, raises
 
-from logprep.input.confluent_kafka_input import ConfluentKafkaInput
 from logprep.output.es_output import ElasticsearchOutput
 from logprep.output.output import CriticalOutputError
 import elasticsearch.helpers
@@ -28,30 +28,8 @@ elasticsearch.helpers.bulk = mock_bulk
 
 
 class TestElasticsearchOutput:
-    default_configuration = {
-        'bootstrap.servers': 'bootstrap1,bootstrap2',
-        'group.id': 'consumer_group',
-        'enable.auto.commit': False,
-        'enable.auto.offset.store': True,
-        'session.timeout.ms': 6000,
-        'default.topic.config': {'auto.offset.reset': 'smallest'},
-        'acks': 'all',
-        'compression.type': 'none',
-        'queue.buffering.max.messages': 31337,
-        'linger.ms': 0
-    }
-
     def setup_method(self, _):
-        self.config = deepcopy(self.default_configuration)
-        self.kafka_input = ConfluentKafkaInput(['bootstrap1', 'bootstrap2'],
-                                               'consumer_topic',
-                                               'consumer_group',
-                                               True)
         self.es_output = ElasticsearchOutput('host', 123, 'default_index', 'error_index', 1, 5000)
-
-    def remove_options(self, *args):
-        for key in args:
-            del self.config[key]
 
     def test_implements_abstract_methods(self):
         try:
@@ -59,11 +37,11 @@ class TestElasticsearchOutput:
         except TypeError as err:
             fail('Must implement abstract methods: %s' % str(err))
 
-    def test_describe_endpoint_returns_kafka_with_first_boostrap_config(self):
+    def test_describe_endpoint_returns_elasticsearch_output(self):
         assert self.es_output.describe_endpoint() == 'Elasticsearch Output: host123'
 
-    def test_store_sends_event_to_expected_topic(self):
-        default_index = 'producer_topic'
+    def test_store_sends_event_to_expected_index(self):
+        default_index = 'target_index'
         event = {'field': 'content'}
         expected = {'field': 'content',
                     '_index': default_index}
@@ -73,8 +51,22 @@ class TestElasticsearchOutput:
 
         assert es_output._message_backlog[0] == expected
 
-    def test_store_custom_sends_event_to_expected_topic(self):
-        custom_index = 'custom_topic'
+    def test_store_sends_event_to_expected_index_with_date_pattern(self):
+        default_index = 'default_index-%{YYYY-MM-DD}'
+        event = {'field': 'content'}
+
+        formatted_date = arrow.now().format('YYYY-MM-DD')
+        expected_index = re.sub(r'%{YYYY-MM-DD}', formatted_date, default_index)
+        expected = {'field': 'content',
+                    '_index': expected_index}
+
+        es_output = ElasticsearchOutput('host', 123, default_index, 'error_index', 1, 5000)
+        es_output.store(event)
+
+        assert es_output._message_backlog[0] == expected
+
+    def test_store_custom_sends_event_to_expected_index(self):
+        custom_index = 'custom_index'
         event = {'field': 'content'}
 
         expected = {'field': 'content',
@@ -111,10 +103,9 @@ class TestElasticsearchOutput:
         del error_document['timestamp']
         del expected['timestamp']
 
-        # assert error_topic == expected
         assert error_document == expected
 
-    def test_create_confluent_settings_contains_expected_values2(self):
+    def test_create_es_output_settings_contains_expected_values(self):
         with raises(CriticalOutputError,
                     match=r'Error storing output document\: Serialization Error'):
             self.es_output.store({'invalid_json': NotJsonSerializableMock(), 'something_valid': 'im_valid!'})
